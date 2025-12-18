358d53adf0d85b577978f440b327213efb87ede6
bernes1 <37117219+bernes1@users.noreply.github.com>
2025-12-18 01:39:57 +0100

    remove

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,7 +5,6 @@
 <template>
     <div class="flex flex-col text-center mb-10">
         <h1 class="text-2xl font-bold mb-10">Contact</h1>
-        <p>Mastodon: <a class="link" href="https://infosec.exchange/@duky" target="_blank" rel="noopener noreferrer">@duky@infosec.exchange</a></p>
         <p>Linkedin: <a class="link" href="https://www.linkedin.com/in/hermanbremnes/" target="_blank" rel="noopener noreferrer">Herman Bremnes</a></p>
     </div>
 </template>
